ffd27db3875ea9f5b740ec57f220a4cf222942a2
Maria Velandia <velandiafernanda17@gmail.com>
2024-01-18 15:12:36 -0500

    feat: configure entry point for cloud

--- app/main.py
+++ app/main.py
@@ -3,10 +3,15 @@ import logging
 from flask import Flask
 from app.router.router import map_routes
 
-if __name__ == "__main__":
+
+def create_app():
     app = Flask(__name__)
     logger = app.logger
     logger.setLevel(logging.ERROR)
-
     map_routes(app)
+    return app
+
+
+if __name__ == "__main__":
+    app = create_app()
     app.run(debug=False, port=8080)
